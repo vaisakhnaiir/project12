6300c8df5be5ef1c4c6eb43f6e8780b2c8d0d603
visakhztg <visakh@ztgmiami.com>
2025-03-28 12:55:40 +0530

    docker changesnow

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ CONTAINER_INIT_STARTUP="CONTAINER_INIT_STARTUP"
 
 echo "Entrypoint file started..."
 
-# Default ACTION to 'init' if not passed
-# ACTION=${ACTION:-init}  # Default to 'init' if ACTION is not set
-
 if [ "$1" = "init" ]; then
     # script that should run on FIRST/INITIAL execution
     echo "Initial Execution [Setup + Migration]"
@@ -14,15 +11,15 @@ if [ "$1" = "init" ]; then
     php artisan config:clear
     php artisan migrate:fresh --seed
     php artisan package:discover --ansi
-     php artisan serve --host 0.0.0.0 --port=${PORT:-8000}
+    php artisan serve --host 0.0.0.0
 elif [ "$1" = "migrate" ]; then
     echo "Do a migration Execution"
 
     php artisan migrate
-     php artisan serve --host 0.0.0.0 --port=${PORT:-8000}
+    php artisan serve --host 0.0.0.0
 else
     # script that should run on > 2nd (NORMAL) execution
     echo "Normal Execition"
 
-    php artisan serve --host 0.0.0.0 --port=${PORT:-8000}
+    php artisan serve --host 0.0.0.0
 fi
